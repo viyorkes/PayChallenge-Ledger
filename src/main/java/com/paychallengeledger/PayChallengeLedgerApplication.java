9b1ccb8a046b3c946c772b54581bb68b85aede46
victor <victorsleiman25@gmail.com>
2024-02-17 14:43:16 -0300

    Initial commit
    Setup project structure and basic configurations

---
+++ src/main/java/com/paychallengeledger/PayChallengeLedgerApplication.java
@@ -0,0 +1,12 @@
+package com.paychallengeledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayChallengeLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayChallengeLedgerApplication.class, args);
+    }
+}

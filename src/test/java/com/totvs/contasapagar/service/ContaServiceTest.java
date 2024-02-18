f3341e9f84e0c18d07daba9d4c7e95cc1a895377
victor <victorsleiman25@gmail.com>
2024-02-17 23:22:40 -0300

    fix_test
    editando teste conta service teste

--- src/test/java/com/totvs/contasapagar/service/ContaServiceTest.java
+++ src/test/java/com/totvs/contasapagar/service/ContaServiceTest.java
@@ -1,21 +1,26 @@
 package com.totvs.contasapagar.service;
 
+
+import com.totvs.contasapagar.application.service.ContaService;
+import com.totvs.contasapagar.application.service.dto.AtualizacaoContaDTO;
 import com.totvs.contasapagar.domain.model.Conta;
 import com.totvs.contasapagar.domain.repository.ContaRepository;
-import com.totvs.contasapagar.application.service.ContaService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.persistence.EntityNotFoundException;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 public class ContaServiceTest {
 
     @Mock
@@ -25,17 +30,50 @@ public class ContaServiceTest {
     private ContaService contaService;
 
     @Test
-    public void salvarConta_DeveRetornarContaSalva() {
+    void salvarConta_DeveSalvarConta() {
         Conta conta = new Conta();
-        conta.setDescricao("Teste");
-        conta.setValor(new BigDecimal("100.00"));
-        // Configure sua conta conforme necessário
 
         when(contaRepository.save(any(Conta.class))).thenReturn(conta);
 
-        Conta contaSalva = contaService.salvarConta(conta);
+        contaService.salvarConta(conta);
 
-        assertThat(contaSalva).isNotNull();
-        verify(contaRepository, times(1)).save(conta);
+        verify(contaRepository).save(conta);
     }
+
+    @Test
+    void atualizarConta_QuandoContaExiste_DeveAtualizarConta() {
+        Long contaId = 1L;
+        Conta contaExistente = new Conta();
+        contaExistente.setId(contaId);
+        contaExistente.setDescricao("Descrição Original");
+
+        AtualizacaoContaDTO dto = new AtualizacaoContaDTO();
+        dto.setDataVencimento(LocalDate.now());
+        dto.setDataPagamento(LocalDate.now());
+        dto.setValor(new BigDecimal("100.00"));
+        dto.setDescricao("Descrição Atualizada");
+        dto.setSituacao(Conta.SituacaoConta.PAGA);
+
+        when(contaRepository.findById(contaId)).thenReturn(Optional.of(contaExistente));
+        when(contaRepository.save(any(Conta.class))).thenReturn(contaExistente);
+
+        contaService.atualizarConta(contaId, dto);
+
+        verify(contaRepository).save(contaExistente);
+        verify(contaRepository).findById(contaId);
+    }
+
+    @Test
+    void atualizarConta_QuandoContaNaoExiste_DeveLancarExcecao() {
+        Long contaId = 1L;
+        AtualizacaoContaDTO dto = new AtualizacaoContaDTO();
+
+        when(contaRepository.findById(contaId)).thenThrow(new EntityNotFoundException("Conta não encontrada"));
+
+        assertThrows(EntityNotFoundException.class, () -> contaService.atualizarConta(contaId, dto));
+
+        verify(contaRepository, never()).save(any(Conta.class));
+    }
+
+
 }
